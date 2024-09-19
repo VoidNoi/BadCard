74750ca288acc671add0837f0c8cac7d26a0a6d4
VoidNoi <voidnoi@proton.me>
2024-09-19 17:06:03 +0200

    Update version

--- BadCard.ino
+++ BadCard.ino
@@ -907,7 +907,7 @@ void bootLogo(){
   display.fillScreen(BLACK);
   
   display.setTextSize(2);
-  String BCVersion = "BadCard v1.6.1";
+  String BCVersion = "BadCard v1.6.2";
 
   display.setCursor(display.width()/2-(BCVersion.length()/2)*letterWidth, display.height()/2 - 50);
   display.println(BCVersion);
